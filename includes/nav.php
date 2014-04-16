73116b2d6eb06b9d9a3cf6d94c761cbfd62bf670
Jon Amar <jon@solomongiles.com>
2014-04-16 19:51:47 +0200

    core styling for nav

---
+++ includes/nav.php
@@ -0,0 +1,72 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="row">
+    <ul class="list-unstyled list-box">
+      <li>
+        <a class="btn btn-primary" href="#">
+          <i class="fa fa-user vr-fa-circled"></i><br>
+          My Account
+        </a>
+      </li>
+      <li>
+        <a class="btn btn-warning" href="#">
+          <i class="fa fa-heart vr-fa-circled"></i><br>
+          My Wishlist
+        </a>
+      </li>
+      <li>
+        <a class="btn btn-success" href="#">
+          <i class="fa fa-unlock-alt vr-fa-circled"></i><br>
+          Login
+        </a>
+      </li>
+      <li class="">
+        <a class="btn btn-info" href="#">
+          <i class="fa fa-flag-checkered vr-fa-circled"></i><br>
+          Checkout
+        </a>
+      </li>
+      <li>
+        <a class="btn btn-danger" href="#">
+          <i class="fa fa-shopping-cart vr-fa-circled"></i><br>
+          My Cart
+        </a>
+      </li>
+      <li class="lb-currency">
+        <div class="btn-group">
+          <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
+            Currency: USD <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">EUR</a></li>
+            <li><a href="#">USD</a></li>
+          </ul>
+        </div>
+      </li>
+      <li>
+        <div class="btn-group">
+          <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">
+            <span class="flag flag-us"></span>English <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#"><span class="flag flag-de"></span>German</a></li>
+            <li><a href="#"><span class="flag flag-hk"></span>Mandarin</a></li>
+            <li><a href="#"><span class="flag flag-cn"></span>Cantonese</a></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+    </div>
+    <div class="row">
+      <ul class="list-inline list-nav">
+        <li><a href="#">New</a></li>
+        <li><a href="#" class="active">Fashion</a></li>
+        <li><a href="#">Beauty</a></li>
+        <li><a href="#">Sport</a></li>
+        <li><a href="#">Lifestyle</a></li>
+        <li><a href="#">Home &amp; Living</a></li>
+        <li><a href="#">Wine &amp; Gourmet</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include 'includes/header.php'; ?>
+<?php include 'includes/nav.php'; ?>
 
     <div class="container">
         <h1 class="text-primary">Ni Ni <small>Actress/Model</small></h1>
@@ -11,6 +12,18 @@
         <p>
           <i class="fa fa-camera-retro"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
+
+        <div class="row">
+          <div class="col-xs-4">
+            test 4
+          </div>
+          <div class="col-xs-4">
+            test 4
+          </div>
+          <div class="col-xs-4">
+            test 4
+          </div>
+        </div>
     </div>
 
 <?php include 'includes/footer.php'; ?>
